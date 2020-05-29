0d40ba96c32b98770475524c4773af523c5a63af
kaibergman <kb625@kent.ac.uk>
2020-05-29 11:20:15 +0100

    added backbone of the server (port listener /scanner)

---
+++ Server.java
@@ -0,0 +1,12 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080 ....");
+        while (true) {
+            final Socket client = server.accept();
+        }
+    }
+}
