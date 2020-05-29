be2c1f636817dda425632d6d019cc8703590aabc
kaibergman <kb625@kent.ac.uk>
2020-05-29 13:39:08 +0100

    added multithreaded request handling

--- Server.java
+++ Server.java
@@ -1,21 +1,34 @@
+/*
+* @author Kai Bergman
+* @version 29/05/2020
+*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
+import java.util.*;
 
-public class Server {
+public class Server extends Thread {
     public static void main(String[] args) throws Exception {
         ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080 ....");
         while (true) {
-            try (Socket socket = server.accept()) {
-                Date now = new Date();
-                String response = "HTTP/1.1 200 OK\r\n\r\n" + now;
-                socket.getOutputStream().write(response.getBytes("UTF-8"));
+            Socket socket = null;
+            try {
+                socket = server.accept();
+
+            } catch (IOException e) {
+
+                System.out.println("Server Stopped." + e);
+                return;
+
             }
+            System.out.println("new client");
+            new Thread(new WorkerRunnable(socket, "new client")).start();
 
         }
     }
+
 }
