fa92ca7f34b8fb24c0c340d4ba8c5464d2b2fbb7
kaibergman <kb625@kent.ac.uk>
2020-05-29 11:40:26 +0100

    server now returns the current date and time.

--- Server.java
+++ Server.java
@@ -3,19 +3,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 public class Server {
     public static void main(String[] args) throws Exception {
         ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080 ....");
         while (true) {
-            Socket clientSocket = server.accept();
-            InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-            BufferedReader reader = new BufferedReader(isr);
-            String line = reader.readLine();
-            while (!line.isEmpty()) {
-                System.out.println(line);
-                line = reader.readLine();
+            try (Socket socket = server.accept()) {
+                Date now = new Date();
+                String response = "HTTP/1.1 200 OK\r\n\r\n" + now;
+                socket.getOutputStream().write(response.getBytes("UTF-8"));
             }
 
         }
